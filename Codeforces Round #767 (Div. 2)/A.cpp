0f7ea7db361151431661cff68a095f42aeb067d9
Frederick Pek <ssixthlhm@gmail.com>
2022-01-23 00:42:02 +0800

    add #767 sols

--- Codeforces Round #767 (Div. 2)/A.cpp
+++ Codeforces Round #767 (Div. 2)/A.cpp
@@ -4,7 +4,20 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < n; i++)
 
 void solve() {
+	int n, k;
+	cin >> n >> k;
 
+	vector<pair<int,int>> v(n);	
+	rep(i, n) cin >> v[i].first;
+	rep(i, n) cin >> v[i].second;
+	sort(v.begin(), v.end());
+
+	for (auto [a, b] : v) {
+		if (k < a) break;
+		k += b;
+	}
+
+	cout << k << endl;
 }
 
 int main() {
--- Codeforces Round #767 (Div. 2)/B.cpp
+++ Codeforces Round #767 (Div. 2)/B.cpp
@@ -1,10 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define ll long long
 #define rep(i, n) for (int i = 0; i < n; i++)
 
 void solve() {
+	int l, r, k;
+	cin >> l >> r >> k;
 
+	if (l==r) {
+		cout << ((l>1) ? "YES" : "NO") << endl;
+		return;
+	}
+
+	
 }
 
 int main() {
