5758f0326011fd7e953df9bba79f117754dd9af2
Frederick Pek <ssixthlhm@gmail.com>
2022-04-09 17:11:50 +0800

    remove redundant files

--- Educational Codeforces Round 126 (Div. 2)/A.cpp
+++ Educational Codeforces Round 126 (Div. 2)/A.cpp
@@ -4,14 +4,16 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < n; i++)
 
 void solve() {
-
+	int a, b;
+	cin >> a >> b;
+	cout << a * b << endl;
 }
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
-
+	
 	int t;
 	cin >> t;
-	rep(i, t) sovle();
+	rep(i, t) solve();
 }
--- Educational Codeforces Round 126 (Div. 2)/B.cpp
+++ Educational Codeforces Round 126 (Div. 2)/B.cpp
@@ -3,11 +3,15 @@ using namespace std;
 
 #define rep(i, n) for (int i = 0; i < n; i++)
 
+void solve() {
+	
+}
+
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 	
-	int n;
-	cin >> n;
-	cout << ((n==2 || n%2)?"NO":"YES") << endl;
+	int t;
+	cin >> t;
+	rep(i, t) solve();
 }
